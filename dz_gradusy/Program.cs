a1f9b47017c61002b36e2d6bb27634948cbde55d
Daniya Bekzhan <Marozhkakim25@gmail.com>
2020-02-28 14:52:24 +0600

    Добавьте файлы проекта.

---
+++ dz_gradusy/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+
+    public class BaseConverter
+    {
+        int temperature;
+        float farengeit;
+        int kelvin;
+
+        public int Temperature
+        {
+            get { return temperature; }
+
+            set
+            {
+                temperature = value;
+            }
+        }
+
+        public float Farengeit
+        {
+            get { return farengeit; }
+
+            set
+            {
+                farengeit = value - 32 * 5 / 9;
+            }
+        }
+
+        public int Kelvin
+        {
+            get { return kelvin; }
+            set
+            {
+                kelvin = value + 273;
+            }
+        }
+
+        public void Info()
+        {
+            Console.WriteLine($"Цельсия:{temperature}, Кельвины:{kelvin}, Фаренгейты:{farengeit}");
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                BaseConverter fg = new BaseConverter();
+                fg.Temperature = 1;
+                fg.Kelvin = 1;
+                fg.Farengeit = 1;
+                fg.Info();
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
